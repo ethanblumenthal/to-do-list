aafd83b22c6fb967ddf0bf35c6017eee032f8da2
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2017-12-08 09:55:06 -0800

    refactored code

--- js/main.js
+++ js/main.js
@@ -2,23 +2,30 @@ var $input = $('#todo');
 var $list = $('.list');
 var $form = $('.form');
 
+// add new item
 $form.submit(function(evt) {
     evt.preventDefault();
     var $newItem = $('<p>').hide();
-    $newItem.append('<input type="checkbox"><i class="glyphicon glyphicon-star"></i><span>' + $input.val() + '</span><i class="glyphicon glyphicon-remove"></i>');
+    $newItem.append('<input type="checkbox">');
+    $newItem.append('<i class="glyphicon glyphicon-star"></i>');
+    $newItem.append('<span>' + $input.val() + '</span>');
+    $newItem.append('<i class="glyphicon glyphicon-remove"></i>');
     $list.prepend($newItem);
-    $newItem.slideDown('slow');    
+    $newItem.slideDown('slow');
     $input.val('');
 });
 
+// mark item complete
 $list.on('click', 'input', function() {
-    $(this).parent().toggleClass('strike');
+    $(this).parent().toggleClass('completed');
 });
 
+// favorite items
 $list.on('click', '.glyphicon-star', function() {
-    $(this).toggleClass('yellow');
+    $(this).toggleClass('active');
 });
 
+// delete items
 $list.on('click', '.glyphicon-remove', function() {
     $(this).parent().slideUp('slow', function() {
         $(this).remove();
