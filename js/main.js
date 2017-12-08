6ec05c78aeb0c7fb1c195c67cbac9aa2dbb67857
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2017-12-07 17:39:23 -0800

    added ability to add list items

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var $input = $('#todo');
+var $list = $('.list');
+var $form = $('.form');
+var $btn = $('.btn');
+
+$form.submit(function(evt) {
+    evt.preventDefault();
+    var $newItem = $('<p>').hide();
+    $newItem.append('<input type="checkbox"><i class="glyphicon glyphicon-star"></i><span>  ' + $input.val() + '</span><i class="glyphicon glyphicon-remove"></i>');
+    $list.prepend($newItem);
+    $newItem.slideDown('slow');    
+    $input.val('');
+});
